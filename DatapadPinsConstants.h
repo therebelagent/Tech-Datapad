659e03dad7dbfda5b277bc7929a3b969705fb847
therebelagent <58261740+therebelagent@users.noreply.github.com>
2021-08-05 11:24:31 -0300

    Delete DatapadPinsConstants.h

--- DatapadPinsConstants.h
+++
@@ -1,9 +0,0 @@
-#define YP A3   //LCD_CS Should be analogic.
-#define XM A2   //LCD_RS Should be analogic.
-#define YM 23   //D1
-#define XP 22   //D0
-
-#define SMALL_WHITE_BUTTON_PIN 30
-#define RED_BUTTON_PIN 31
-#define WHITE_BUTTON_PIN 32
-#define YELLOW_BUTTON_PIN 33
